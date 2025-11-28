275ced7069c4dda0c18877c74f791614fe0382ec
Al Zaplin <zaplin.dev@gmail.com>
2025-11-28 04:38:51 +0300

    Feat: (Burger menu) Mobile nav menu closes after clicking on one of the links

--- app/js/scripts/burger-menu.js
+++ app/js/scripts/burger-menu.js
@@ -1,9 +1,18 @@
 export default function burgerMenuFunc() {
   const menuButton = document.querySelector(".hamburger");
   const menuListMobile = document.querySelector(".header__menu-list");
+  const menuMobileLinks = menuListMobile.querySelectorAll("a");
 
-  menuButton.addEventListener("click", () => {
+  function toggleMobileMenu() {
     menuButton.classList.toggle("is-active");
     menuListMobile.classList.toggle("menu--open");
-  });
+  }
+
+  menuButton.addEventListener("click", () => toggleMobileMenu());
+
+  menuMobileLinks.forEach((link) =>
+    link.addEventListener("click", () =>
+      setTimeout(() => toggleMobileMenu(), 1000)
+    )
+  );
 }
